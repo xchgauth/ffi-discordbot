9f228653d0a5149200f5f83f4d73aedc8525f712
xchgauth <xchgauth@gmail.com>
2026-01-02 11:58:23 -0500

    Add files via upload

---
+++ src/lib/core/queriec.h
@@ -0,0 +1,26 @@
+#ifndef QUERIEC_H
+#define QUERIEC_H
+
+#define QUERIEC_ADDITIONAL_LETTERS_SIZE 2
+
+#define QUERIEC_ERROR_NOMEM -1
+#define QUERIEC_OK 0
+
+#include "attributes.h"
+
+struct queriec {
+    int state;
+    size_t size;
+    size_t offset;
+};
+
+void
+queriec_init(struct queriec *queriec, size_t size);
+
+int queriec_snprintf_add(struct queriec *queriec, char *query, const char key[], size_t keySize, 
+                         char buffer[], size_t bufferLen, const char *format, ...) PRINTF_LIKE(7, 8);
+
+int
+queriec_add(struct queriec *queriec, char *query, char key[], size_t keySize, char value[], size_t valueSize);
+
+#endif
